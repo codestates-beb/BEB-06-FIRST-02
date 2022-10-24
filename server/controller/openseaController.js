4fc5763ec26aebf04d28fea51f012fb6d46b84e1
wonhyeok.choi <yuanhe1995@naver.com>
2022-10-24 13:56:32 +0900

    wonhyeok/더미데이터 업데이트/ 민팅 기능 추가

--- server/controller/openseaController.js
+++ server/controller/openseaController.js
@@ -3,24 +3,31 @@ const fs = require("fs");
 const DB = require("../DB/nftList.json")
 
 const _data = {
-    price : "0 ETH",
-    token_id: "",
-    name: "",
-    traits: "",
-    image: "",
-    theme: ""
+    "name": "",
+    "token_id": "",
+    "traits": "",
+    "image": "",
+    "theme": "",
+    "writer": "",
+    "price": ""
 }
+
+// price/writer/theme
+
 /* 
     Buy 하면 NFT 판매 데이터 req로 받고 DB(nftList.json)맨 앞에 데이터 추가
 */
 // http://localhost:3001/opesea/listing
 module.exports = {
     listing : (req, res) => {
+
+    // req
+
     fs.readFile('./DB/nftList.json','utf8',(err,data) => {
         if(err) throw err;
         let obj = JSON.parse(data)
         obj.unshift(_data)
-        let json = JSON.stringify(obj);
+        let json = JSON.stringify(obj,null,4);
         fs.writeFile('./DB/nftList.json',json,'utf8',(err,result) => {
             if (err) throw err;
         })
